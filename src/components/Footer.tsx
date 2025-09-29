d19ec209a014e9fedf6d942175bd6022f8e1d8e3
bahodirxro1-cell <bahodirxro1@gmail.com>
2025-09-29 11:57:37 +0500

    footer icon to

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
-'use client';
-
+"use client";
 
 import { memo } from "react";
 import { Facebook, Linkedin, Instagram, Mail } from "lucide-react";
@@ -11,7 +10,10 @@ const Footer = () => {
 
   return (
     <div className="bg-[#1A1A1A] text-white text-center py-[49px]">
-      <div onClick={smoothSc} className="mb-[43px] flex flex-col items-center justify-center">
+      <div
+        onClick={smoothSc}
+        className="mb-[43px] flex flex-col items-center justify-center"
+      >
         <div className="flex flex-col items-center mb-2 cursor-pointer">
           <span className="block w-2 h-2 border-t-2 border-l-2 rotate-45 border-white" />
           <span className="block w-2 h-2 border-t-2 border-l-2 rotate-45 border-white" />
@@ -20,10 +22,18 @@ const Footer = () => {
       </div>
 
       <div className="flex justify-center gap-[28px] mb-[43px]">
-        <Facebook />
-        <Linkedin />
-        <Instagram />
-        <Mail />
+        <a href="https://www.facebook.com/">
+          <Facebook />
+        </a>
+        <a href="https://www.linkedin.com/in/bahodir-nabijanov-816804350/">
+          <Linkedin />
+        </a>
+        <a href="https://instagram.com/nabijanov_uz">
+          <Instagram />
+        </a>
+        <a href="mailto:bahodirnabijanov782@gmail.com">
+          <Mail />
+        </a>
       </div>
       <div>
         <p>@2025 Bahodir Nabijanov All Rights Reserved.</p>
